a2817937ed979bd77e8bf53529ab1d662d599bc5
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-10-27 14:46:24 +0530

    Day07

---
+++ day07-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RefHookExample from './RefHookExample';
+
+function App() {
+  return (
+    <div className="App">
+      <RefHookExample/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day07-app/src/RefHookExample.js
@@ -0,0 +1,21 @@
+import {useRef, useState} from 'react';
+
+const RefHookExample = () => {
+    const [message, setMessage] = useState("");
+    const nameText = useRef(); //React.createRef();
+    const btn = useRef();
+
+    const buttonClicked = () => {
+        let name = nameText.current.value;
+        setMessage(`Hi ${name}`);
+        btn.current.style.backgroundColor = "yellow";
+    };
+
+    return (<div>
+        <input type="text" placeholder="name" ref={nameText}></input><br/>
+        <button ref={btn} onClick={buttonClicked}>Click</button>
+        <h1>{message}</h1>
+    </div>)
+};
+
+export default RefHookExample;
